bcf1c22f649ec8dfbc0782f8e46bbee790455735
Aniket Panse <aniket@getpostman.com>
2015-07-14 15:42:55 +0530

    fixed folders not having names, added ability to process path variables

--- tests/unit/converter-v1-to-v2-spec.js
+++ tests/unit/converter-v1-to-v2-spec.js
@@ -11,7 +11,7 @@ var expect = require('expect.js'),
 /* global describe, it */
 describe('v1.0.0 ==> v2.0.0', function () {
     var converter = require('../../lib/converters/converter-v1-to-v2'),
-        schema = require('../../lib/schemas/json/collection/v2.0.0-draft.1/index'),
+        schema = require('../../lib/schemas/json/collection/v2.0.0-draft.2/index'),
         examplesDir = path.join(__dirname, '../../examples/v1');
 
     describe('sample conversions', function () {
@@ -20,6 +20,9 @@ describe('v1.0.0 ==> v2.0.0', function () {
             it('must create a valid V2 collection from ' + sampleName + '.json', function () {
                 converter.convert(sample, {}, function (err, converted) {
                     var result = tv4.validate(converted, schema);
+                    if (!result) {
+                        console.log(JSON.stringify(tv4.error, null, 4)); // Helps debug on CI
+                    }
                     expect(result).to.be(true);
                 });
             });
