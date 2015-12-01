b0f94a5f47e7cb75d6fe34eadb8b45d9741b91d5
Aniket Panse <aniket@getpostman.com>
2015-12-01 13:40:19 +0530

    added a test to ensure that we never add leading slashes to urls

--- tests/unit/converter-v1-to-v2-spec.js
+++ tests/unit/converter-v1-to-v2-spec.js
@@ -5,6 +5,7 @@
 var expect = require('expect.js'),
     requireAll = require('require-all'),
     path = require('path'),
+    util = require('../../lib/util'),
     tv4 = require('tv4'),
     _ = require('lodash');
 
@@ -17,15 +18,25 @@ describe('v1.0.0 ==> v2.0.0', function () {
     describe('sample conversions', function () {
         var samples = requireAll(examplesDir);
         _.map(samples, function (sample, sampleName) {
-            it('must create a valid V2 collection from ' + sampleName + '.json', function () {
+            it('must create a valid V2 collection from ' + sampleName + '.json', function (done) {
                 converter.convert(sample, {}, function (err, converted) {
                     var result = tv4.validate(converted, schema);
                     if (!result) {
                         console.log(JSON.stringify(tv4.error, null, 4)); // Helps debug on CI
                     }
                     expect(result).to.be(true);
+                    done();
                 });
             });
         });
+    });
+
+    it('must not add leading "//" when a variable is present at the beginning of the url', function (done) {
+        var sample = require(path.join(examplesDir, 'proper-url-parsing.json'));
+        converter.convert(sample, {}, function (err, converted) {
+            expect(util.stringStartsWith(converted.items[0].request.url.href, '//')).to.be(false);
+            expect(util.stringEndsWith(converted.items[0].request.url.href, '?')).to.be(false);
+        });
+        done();
     });
 });
