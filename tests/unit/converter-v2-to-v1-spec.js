9f41f010456145995e36dcb093c4505e4fa8fe72
Aniket Panse <aniket@getpostman.com>
2016-06-17 13:05:51 +0530

    revert to the older version of v1 to v2 converter, so that the next version will have no breaking changes

--- tests/unit/converter-v2-to-v1-spec.js
+++ tests/unit/converter-v2-to-v1-spec.js
@@ -78,7 +78,7 @@ describe('v2.0.0 ==> v1.0.0', function () {
         });
     });
 
-    it('must be compatible with both v2.0.0 and v2.1.0 formats', function () {
+    it.skip('must be compatible with both v2.0.0 and v2.1.0 formats', function () {
         var samples_2_1_0 = requireAll(path.join(__dirname, '../../examples/v2.1.0')),
             samples_2_0_0 = requireAll(path.join(__dirname, '../../examples/v2.0.0')),
 
