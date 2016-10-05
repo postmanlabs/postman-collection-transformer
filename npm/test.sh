20f69539821a399b873d54d3ad49d4ad8757b3fe
kunagpal <kunagpal@gmail.com>
2016-10-05 22:06:08 +0530

    Renamed test files

--- npm/test.sh
+++ npm/test.sh
@@ -22,16 +22,16 @@ echo -en "\033[0m\033[2m";
 
 # run jscs
 echo -e "jscs v`jscs --version`\033[0m\n";
-jscs index.js lib bin tests/unit
+jscs index.js lib bin test
 
 # run jshint
 echo # blank line
 jshint -v;
-jshint index.js lib bin tests/unit --show-non-errors --verbose;
+jshint index.js lib bin test --show-non-errors --verbose;
 
 echo -n "No lint errors found.";
 
 # run mocha tests
 echo -e "\n\n\033[93mRunning unit tests...\033[0m";
 echo -en "\033[0m\033[2mmocha `mocha --version`\033[0m";
-mocha -t 10000 tests/unit/*-spec.js
+mocha -t 10000 test
