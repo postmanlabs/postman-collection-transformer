7c25082349a7e4837e38d98f4aa0e7ad22f638bb
Kunal Nagpal <kunagpal@gmail.com>
2017-05-11 16:00:14 +0530

    :bug: Fixed disabled header v1 <-> v2 transformations

--- tests/unit/fixtures/single-request.js
+++ tests/unit/fixtures/single-request.js
@@ -39,7 +39,7 @@ module.exports = {
         rawModeData: null,
         descriptionFormat: null,
         description: "This endpoint is used to get the `access_token`\n\nIt requires the following secret client credentials to be sent as part of the form body along with the `authentication code` obtained as part of the `redirect_uri` from the previous request.\n\n> code: xWnkliVQJURqB2x1\n>\n> grant_type: authorization_code\n>\n> redirect_uri: https://www.getpostman.com/oauth2/callback\n>\n> client_id: abc123\n>\n> client_secret: ssh-secret\n\nIf the correct credentials are not passed, the server returns with a `401 Unauthorized` response.",
-        headers: "A:B\nC:D",
+        headers: "A:B\nC:D\n// E: F",
         method: "POST",
         pathVariables: {},
         url: "https://yo.postman.wtf/oauth2/token",
@@ -361,7 +361,8 @@ module.exports = {
             method: "POST",
             header: [
                 { key: 'A', value: 'B', description: '' },
-                { key: 'C', value: 'D', description: '' }
+                { key: 'C', value: 'D', description: '' },
+                { key: 'E', value: 'F', description: '', disabled: true }
             ],
             body: {
                 mode: "formdata",
