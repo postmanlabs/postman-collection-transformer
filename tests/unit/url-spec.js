6e4191eb1182079cf396d003b75d407609c22192
Aniket Panse <aniketpanse@gmail.com>
2017-06-09 15:23:35 +0530

    changed the tests to be unit tests instead of integration tests

--- tests/unit/url-spec.js
+++ tests/unit/url-spec.js
@@ -3,69 +3,47 @@
  */
 
 var expect = require('expect.js'),
-    transformer = require('../../index');
+    url = require('../../lib/url');
 
 /* global describe, it */
-describe('url param handling', function () {
-    describe('v1.0.0 to v2.0.0', function () {
-        it('should work as intended', function (done) {
-            var fixture = {
-                    id: 'c95c8ffd-1fc0-7c46-0522-979e99b08ba0',
-                    headers: '',
-                    headerData: [],
-                    url: 'postman-echo.com/get?user_email=fred@gmail.com',
-                    queryParams: [
-                        {
-                            key: 'user_email',
-                            value: 'fred@gmail.com',
-                            equals: true,
-                            description: '',
-                            enabled: true
-                        }
-                    ],
-                    pathVariables: {},
-                    pathVariableData: [],
-                    preRequestScript: null,
-                    method: 'GET',
-                    collectionId: 'b332cead-12f6-ca88-b706-2fdd3377f88d',
-                    data: null,
-                    dataMode: 'params',
-                    name: 'postman-echo.com/get?user_email=fred@gmail.com',
-                    description: '',
-                    descriptionFormat: 'html',
-                    time: 1496989401291,
-                    version: 2,
-                    responses: [],
-                    tests: '',
-                    currentHelper: 'normal',
-                    helperAttributes: {}
-                },
-                options = {
-                    inputVersion: '1.0.0',
-                    outputVersion: '2.0.0',
-                    retainIds: true
-                };
+describe('url', function () {
+    describe('parsing', function () {
+        it('should handle `@` character', function (done) {
+            var fixture = 'postman-echo.com/get?user_email=fred@gmail.com',
+                parsed;
 
-            transformer.convertSingle(fixture, options, function (err, converted) {
-                expect(err).to.not.be.ok();
+            parsed = JSON.parse(JSON.stringify(url.parse(fixture)));
 
-                // remove `undefined` properties for testing
-                converted = JSON.parse(JSON.stringify(converted));
-
-                expect(converted.request.url).to.eql({
-                    raw: 'postman-echo.com/get?user_email=fred@gmail.com',
-                    host: ['postman-echo', 'com'],
-                    path: ['get'],
-                    query: [{
-                        key: 'user_email',
-                        value: 'fred@gmail.com',
-                        equals: true,
-                        description: ''
-                    }],
-                    variable: []
-                });
-                done();
+            expect(parsed).to.eql({
+                raw: 'postman-echo.com/get?user_email=fred@gmail.com',
+                host: ['postman-echo', 'com'],
+                path: ['get'],
+                query: [{
+                    key: 'user_email',
+                    value: 'fred@gmail.com'
+                }]
             });
+            done();
+        });
+    });
+
+    describe('unparsing', function () {
+        it('should handle the `@` character', function (done) {
+            var fixture = {
+                raw: 'postman-echo.com/get?user_email=fred@gmail.com',
+                host: ['postman-echo', 'com'],
+                path: ['get'],
+                query: [{
+                    key: 'user_email',
+                    value: 'fred@gmail.com',
+                    equals: true,
+                    description: ''
+                }],
+                variable: []
+            };
+
+            expect(url.unparse(fixture)).to.eql('postman-echo.com/get?user_email=fred@gmail.com');
+            done();
         });
     });
 });
