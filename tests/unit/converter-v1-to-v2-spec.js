6678cb5daa430565d0e524c423eee4439c981f06
Kunal Nagpal <kunagpal@gmail.com>
2017-07-18 17:19:14 +0530

    Added v1 -> v2 unit test for request body files

--- tests/unit/converter-v1-to-v2-spec.js
+++ tests/unit/converter-v1-to-v2-spec.js
@@ -103,4 +103,25 @@ describe('v1.0.0 to v2.0.0', function () {
             });
         });
     });
+
+    describe('request file body', function () {
+        it('should correctly handle request file bodies whilst converting from v1 to v2', function (done) {
+            var fixture = require('./fixtures/request-body-file'),
+                options = {
+                    inputVersion: '1.0.0',
+                    outputVersion: '2.0.0',
+                    retainIds: true
+                };
+
+            transformer.convert(fixture.v1, options, function (err, converted) {
+                expect(err).to.not.be.ok();
+
+                // remove `undefined` properties for testing
+                converted = JSON.parse(JSON.stringify(converted));
+
+                expect(converted).to.eql(fixture.v2);
+                done();
+            });
+        });
+    });
 });
