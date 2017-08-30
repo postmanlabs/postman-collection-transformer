47b730b9656195aa1ccfa04f78b9cb872c0efa7b
Kunal Nagpal <kunagpal@gmail.com>
2017-08-30 19:53:07 +0530

    Shifted existing schema tests to categorized folder

--- test/schema/v1.0.0/converter-v1-to-v2.test.js
+++ test/schema/v1.0.0/converter-v1-to-v2.test.js
@@ -11,10 +11,10 @@ var expect = require('chai').expect,
 
 /* global describe, it, before */
 describe('v1.0.0 ==> v2.0.0', function () {
-    var converter = require('../../lib/converters/converter-v1-to-v2'),
-        reverseConverter = require('../../lib/converters/converter-v2-to-v1'),
-        schemaUrl = require('../../lib/constants').SCHEMA_V2_URL,
-        examplesDir = path.join(__dirname, '../../examples/v1.0.0');
+    var converter = require('../../../lib/converters/v1.0.0/converter-v1-to-v2'),
+        reverseConverter = require('../../../lib/converters/v2.0.0/converter-v2-to-v1'),
+        schemaUrl = require('../../../lib/constants').SCHEMA_V2_URL,
+        examplesDir = path.join(__dirname, '../../../examples/v1.0.0');
 
     describe('sample conversions', function () {
         var schema,
@@ -83,20 +83,20 @@ describe('v1.0.0 ==> v2.0.0', function () {
 
     describe('Exceptional cases', function () {
         it('should handle the edge case of "data" vs "rawModeData"', function () {
-            var v1 = require('../../examples/v1.0.0/simplest.json'),
+            var v1 = require('../../../examples/v1.0.0/simplest.json'),
                 v2 = converter.convert(v1);
             expect(v2.item[0].request.body.raw).to.eql('something');
         });
 
         it('should strip out all request and folder ids by default', function () {
-            var v1 = require('../../examples/v1.0.0/simplest.json'),
+            var v1 = require('../../../examples/v1.0.0/simplest.json'),
                 v2 = JSON.parse(JSON.stringify(converter.convert(v1)));
             expect(v2.item[0]).to.not.have.property('id');
             expect(v2.item[0]).to.not.have.property('_postman_id');
         });
 
         it('should retain all request and folder ids if asked to', function () {
-            var v1 = require('../../examples/v1.0.0/simplest.json'),
+            var v1 = require('../../../examples/v1.0.0/simplest.json'),
                 v2 = JSON.parse(JSON.stringify(converter.convert(v1, {
                     retainIds: true
                 })));
@@ -104,7 +104,7 @@ describe('v1.0.0 ==> v2.0.0', function () {
         });
 
         it('should mark commented out headers as disabled', function () {
-            var v1 = require('../../examples/v1.0.0/disabledheaders.json'),
+            var v1 = require('../../../examples/v1.0.0/disabledheaders.json'),
                 v2 = JSON.parse(JSON.stringify(converter.convert(v1, {
                     retainIds: true
                 })));
@@ -112,7 +112,7 @@ describe('v1.0.0 ==> v2.0.0', function () {
         });
 
         it('should not set default request body for requests with no data', function () {
-            var v1 = require('../../examples/v1.0.0/emptydata.json'),
+            var v1 = require('../../../examples/v1.0.0/emptydata.json'),
                 v2 = JSON.parse(JSON.stringify(converter.convert(v1, {
                     retainIds: true
                 })));
@@ -121,8 +121,8 @@ describe('v1.0.0 ==> v2.0.0', function () {
     });
 
     describe('Binary File reference', function () {
-        it('should be converted to v2 correctly', function () {
-            var v1 = require('../../examples/v1.0.0/binary-upload.json'),
+        it('should be converted to v2.0.0 correctly', function () {
+            var v1 = require('../../../examples/v1.0.0/binary-upload.json'),
                 v2 = JSON.parse(JSON.stringify(converter.convert(v1, {
                     retainIds: true
                 })));
@@ -137,6 +137,7 @@ describe('v1.0.0 ==> v2.0.0', function () {
             var converted = JSON.parse(JSON.stringify(converter.convert(malformedJson))),
                 reverted = JSON.parse(JSON.stringify(reverseConverter.convert(converted)));
 
+            console.log(JSON.stringify(reverted, null, 4));
             expect(reverted.order).to.have.length(1);
             expect(reverted.folders_order).to.have.length(2);
 
