0b014955c9be36ecc758b45a7a3b677d1461e6fa
Kunal Nagpal <kunagpal@gmail.com>
2017-10-10 01:04:50 +0530

    Updated unit test fixtures for schema compliance

--- test/unit/fixtures/nested-entities.js
+++ test/unit/fixtures/nested-entities.js
@@ -6,26 +6,86 @@ module.exports = {
         order: [
             'f619a417-7ab7-d28b-e75e-f110b5862eb6'
         ],
+        variables: [{
+            id: 'collection',
+            value: 'variable',
+            type: 'string',
+            description: 'This is a collection level variable'
+        }],
         folders: [
             {
                 name: 'F1',
                 order: [
                     'a742022f-ff24-18c5-6e8b-0c4e7d7aee08'
                 ],
-                preRequestScript: 'console.log("Folder level pre request script");',
-                tests: 'console.log("Folder level test script");',
-                currentHelper: 'digestAuth',
-                helperAttributes: {
-                    id: 'digest',
-                    algorithm: 'MD5',
-                    username: 'postman',
-                    realm: '{{echo_digest_realm}}',
-                    password: 'password',
-                    nonce: '{{echo_digest_nonce}}',
-                    nonceCount: '',
-                    clientNonce: '',
-                    opaque: '',
-                    qop: ''
+                variables: [{
+                    id: 'folder',
+                    value: 'variable',
+                    type: 'string',
+                    description: 'This is a folder level variable'
+                }],
+                events: [{
+                    listen: 'test',
+                    script: {
+                        type: 'text/javascript',
+                        exec: ['console.log("Folder level test script");']
+                    }
+                }, {
+                    listen: 'prerequest',
+                    script: {
+                        type: 'text/javascript',
+                        exec: ['console.log("Folder level pre request script");']
+                    }
+                }],
+                auth: {
+                    type: 'digest',
+                    digest: [
+                        {
+                            key: 'algorithm',
+                            value: 'MD5',
+                            type: 'string'
+                        },
+                        {
+                            key: 'username',
+                            value: 'postman',
+                            type: 'string'
+                        },
+                        {
+                            key: 'realm',
+                            value: '{{echo_digest_realm}}',
+                            type: 'string'
+                        },
+                        {
+                            key: 'password',
+                            value: 'password',
+                            type: 'string'
+                        },
+                        {
+                            key: 'nonce',
+                            value: '{{echo_digest_nonce}}',
+                            type: 'string'
+                        },
+                        {
+                            key: 'nonceCount',
+                            value: '',
+                            type: 'string'
+                        },
+                        {
+                            key: 'clientNonce',
+                            value: '',
+                            type: 'string'
+                        },
+                        {
+                            key: 'opaque',
+                            value: '',
+                            type: 'string'
+                        },
+                        {
+                            key: 'qop',
+                            value: '',
+                            type: 'string'
+                        }
+                    ]
                 },
                 folders_order: [],
                 id: '5be148b3-0081-5a59-5273-9014161b0292'
@@ -34,24 +94,94 @@ module.exports = {
         folders_order: [
             '5be148b3-0081-5a59-5273-9014161b0292'
         ],
-        preRequestScript: 'console.log("Collection level pre request script");',
-        tests: 'console.log("Collection level test script");',
-        currentHelper: 'awsSigV4',
-        helperAttributes: {
-            id: 'awsSigV4',
-            accessKey: '{{id}}',
-            secretKey: '{{key}}',
-            region: 'us-east-1',
-            service: 'iam',
-            sessionToken: ''
+        events: [
+            {
+                listen: 'test',
+                script: {
+                    type: 'text/javascript',
+                    exec: [
+                        'console.log("Collection level test script");'
+                    ]
+                }
+            },
+            {
+                listen: 'prerequest',
+                script: {
+                    type: 'text/javascript',
+                    exec: [
+                        'console.log("Collection level pre request script");'
+                    ]
+                }
+            }
+        ],
+        auth: {
+            type: 'awsv4',
+            awsv4: [
+                {
+                    key: 'accessKey',
+                    value: '{{id}}',
+                    type: 'string'
+                },
+                {
+                    key: 'id',
+                    value: 'awsSigV4',
+                    type: 'string'
+                },
+                {
+                    key: 'region',
+                    value: 'us-east-1',
+                    type: 'string'
+                },
+                {
+                    key: 'secretKey',
+                    value: '{{key}}',
+                    type: 'string'
+                },
+                {
+                    key: 'service',
+                    value: 'iam',
+                    type: 'string'
+                },
+                {
+                    key: 'sessionToken',
+                    value: '',
+                    type: 'string'
+                }
+            ]
         },
         requests: [
             {
                 id: 'a742022f-ff24-18c5-6e8b-0c4e7d7aee08',
+                variables: [{
+                    id: 'request',
+                    value: 'variable',
+                    type: 'string',
+                    description: 'This is a request level variable'
+                }],
                 headers: '',
                 headerData: [],
                 url: 'https://postman-echo.com/get',
                 queryParams: [],
+                events: [
+                    {
+                        listen: 'test',
+                        script: {
+                            type: 'text/javascript',
+                            exec: [
+                                'console.log("Request level test script");'
+                            ]
+                        }
+                    },
+                    {
+                        listen: 'prerequest',
+                        script: {
+                            type: 'text/javascript',
+                            exec: [
+                                'console.log("Request level pre request script");'
+                            ]
+                        }
+                    }
+                ],
                 preRequestScript: 'console.log("Request level pre request script");',
                 pathVariableData: [],
                 rawModeData: '',
@@ -59,6 +189,21 @@ module.exports = {
                 data: [],
                 dataMode: 'raw',
                 tests: 'console.log("Request level test script");',
+                auth: {
+                    type: 'basic',
+                    basic: [
+                        {
+                            key: 'username',
+                            value: '{{username}}',
+                            type: 'string'
+                        },
+                        {
+                            key: 'password',
+                            value: '{{token}}',
+                            type: 'string'
+                        }
+                    ]
+                },
                 currentHelper: 'basicAuth',
                 helperAttributes: {
                     id: 'basic',
@@ -71,10 +216,36 @@ module.exports = {
             },
             {
                 id: 'f619a417-7ab7-d28b-e75e-f110b5862eb6',
+                variables: [{
+                    id: 'request',
+                    value: 'variable',
+                    type: 'string',
+                    description: 'This is a request level variable'
+                }],
                 headers: '',
                 headerData: [],
                 url: 'https://postman-echo.com/get',
                 queryParams: [],
+                events: [
+                    {
+                        listen: 'test',
+                        script: {
+                            type: 'text/javascript',
+                            exec: [
+                                'console.log("Request level test script");'
+                            ]
+                        }
+                    },
+                    {
+                        listen: 'prerequest',
+                        script: {
+                            type: 'text/javascript',
+                            exec: [
+                                'console.log("Request level pre request script");'
+                            ]
+                        }
+                    }
+                ],
                 preRequestScript: 'console.log("Request level pre request script");',
                 pathVariableData: [],
                 method: 'GET',
@@ -82,6 +253,56 @@ module.exports = {
                 dataMode: 'raw',
                 rawModeData: '',
                 tests: 'console.log("Request level test script");',
+                auth: {
+                    type: 'oauth1',
+                    oauth1: [
+                        {
+                            key: 'consumerKey',
+                            value: 'RKCGzna7bv9YD57c',
+                            type: 'string'
+                        },
+                        {
+                            key: 'consumerSecret',
+                            value: 'D+EdQ-gs$-%@2Nu7',
+                            type: 'string'
+                        },
+                        {
+                            key: 'token',
+                            value: '',
+                            type: 'string'
+                        },
+                        {
+                            key: 'tokenSecret',
+                            value: '',
+                            type: 'string'
+                        },
+                        {
+                            key: 'signatureMethod',
+                            value: 'HMAC-SHA1',
+                            type: 'string'
+                        },
+                        {
+                            key: 'timestamp',
+                            value: '1500452534',
+                            type: 'string'
+                        },
+                        {
+                            key: 'nonce',
+                            value: 'S0kXloMHurS',
+                            type: 'string'
+                        },
+                        {
+                            key: 'version',
+                            value: '1.0',
+                            type: 'string'
+                        },
+                        {
+                            key: 'realm',
+                            value: '',
+                            type: 'string'
+                        }
+                    ]
+                },
                 currentHelper: 'oAuth1',
                 helperAttributes: {
                     id: 'oAuth1',
@@ -102,13 +323,18 @@ module.exports = {
         ]
     },
     v2: {
-        variables: [],
         info: {
             name: 'nested-entities',
             _postman_id: '371e9fa3-97d3-ca11-07c0-836a72a325c2',
             description: 'A simple collection to elucidate nested auth helpers, variables, and scripts',
             schema: 'https://schema.getpostman.com/json/collection/v2.0.0/collection.json'
         },
+        variable: [{
+            id: 'collection',
+            value: 'variable',
+            type: 'string',
+            description: 'This is a collection level variable'
+        }],
         auth: {
             type: 'awsv4',
             awsv4: {
@@ -144,6 +370,12 @@ module.exports = {
             {
                 _postman_id: '5be148b3-0081-5a59-5273-9014161b0292',
                 name: 'F1',
+                variable: [{
+                    id: 'folder',
+                    value: 'variable',
+                    type: 'string',
+                    description: 'This is a folder level variable'
+                }],
                 auth: {
                     type: 'digest',
                     digest: {
@@ -182,6 +414,12 @@ module.exports = {
                     {
                         _postman_id: 'a742022f-ff24-18c5-6e8b-0c4e7d7aee08',
                         name: 'Basic',
+                        variable: [{
+                            id: 'request',
+                            value: 'variable',
+                            type: 'string',
+                            description: 'This is a request level variable'
+                        }],
                         event: [
                             {
                                 listen: 'test',
@@ -207,7 +445,6 @@ module.exports = {
                                 type: 'basic',
                                 basic: {
                                     username: '{{username}}',
-                                    showPassword: false,
                                     password: '{{token}}'
                                 }
                             },
@@ -226,6 +463,12 @@ module.exports = {
             {
                 _postman_id: 'f619a417-7ab7-d28b-e75e-f110b5862eb6',
                 name: 'OAuth1',
+                variable: [{
+                    id: 'request',
+                    value: 'variable',
+                    type: 'string',
+                    description: 'This is a request level variable'
+                }],
                 event: [
                     {
                         listen: 'test',
@@ -280,27 +523,33 @@ module.exports = {
             description: 'A simple collection to elucidate nested auth helpers, variables, and scripts',
             schema: 'https://schema.getpostman.com/json/collection/v2.1.0/collection.json'
         },
+        variable: [{
+            id: 'collection',
+            value: 'variable',
+            type: 'string',
+            description: 'This is a collection level variable'
+        }],
         auth: {
             type: 'awsv4',
             awsv4: [
-                {
-                    key: 'id',
-                    value: 'awsSigV4',
-                    type: 'string'
-                },
                 {
                     key: 'accessKey',
                     value: '{{id}}',
                     type: 'string'
                 },
                 {
-                    key: 'secretKey',
-                    value: '{{key}}',
+                    key: 'id',
+                    value: 'awsSigV4',
                     type: 'string'
                 },
                 {
                     key: 'region',
                     value: 'us-east-1',
+                    type: 'string'
+                },
+                {
+                    key: 'secretKey',
+                    value: '{{key}}',
                     type: 'string'
                 },
                 {
@@ -339,6 +588,12 @@ module.exports = {
             {
                 _postman_id: '5be148b3-0081-5a59-5273-9014161b0292',
                 name: 'F1',
+                variable: [{
+                    id: 'folder',
+                    value: 'variable',
+                    type: 'string',
+                    description: 'This is a folder level variable'
+                }],
                 auth: {
                     type: 'digest',
                     digest: [
@@ -386,10 +641,6 @@ module.exports = {
                             key: 'qop',
                             value: '',
                             type: 'string'
-                        },
-                        {
-                            key: 'disableRetryRequest',
-                            type: 'any'
                         }
                     ]
                 },
@@ -416,6 +667,12 @@ module.exports = {
                 item: [
                     {
                         _postman_id: 'a742022f-ff24-18c5-6e8b-0c4e7d7aee08',
+                        variable: [{
+                            id: 'request',
+                            value: 'variable',
+                            type: 'string',
+                            description: 'This is a request level variable'
+                        }],
                         name: 'Basic',
                         event: [
                             {
@@ -450,15 +707,6 @@ module.exports = {
                                         key: 'password',
                                         value: '{{token}}',
                                         type: 'string'
-                                    },
-                                    {
-                                        key: 'saveHelperData',
-                                        type: 'any'
-                                    },
-                                    {
-                                        key: 'showPassword',
-                                        type: 'boolean',
-                                        value: false
                                     }
                                 ]
                             },
@@ -481,6 +729,12 @@ module.exports = {
             },
             {
                 _postman_id: 'f619a417-7ab7-d28b-e75e-f110b5862eb6',
+                variable: [{
+                    id: 'request',
+                    value: 'variable',
+                    type: 'string',
+                    description: 'This is a request level variable'
+                }],
                 name: 'OAuth1',
                 event: [
                     {
@@ -550,18 +804,6 @@ module.exports = {
                                 key: 'realm',
                                 value: '',
                                 type: 'string'
-                            },
-                            {
-                                key: 'addParamsToHeader',
-                                type: 'any'
-                            },
-                            {
-                                key: 'autoAddParam',
-                                type: 'any'
-                            },
-                            {
-                                key: 'addEmptyParamsToSign',
-                                type: 'any'
                             }
                         ]
                     },
