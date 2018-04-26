621b5c6f98fb79812df54ac4cf04d58975c0f7a9
Kunal Nagpal <kunagpal@gmail.com>
2018-04-26 19:53:30 +0530

    Updated v2.1-v1 tests for new collection id retain behaviour :white_check_mark:

--- test/unit/v2.1.0/converter-v21-to-v1.test.js
+++ test/unit/v2.1.0/converter-v21-to-v1.test.js
@@ -548,7 +548,7 @@ describe('v2.1.0 to v1.0.0', function () {
             });
         });
 
-        it('should handle IDs correctly when set to false', function () {
+        it('should handle IDs correctly when set to false (collection id should be retained)', function () {
             transformer.convert({
                 info: {_postman_id: 'R1'},
                 item: [
@@ -562,7 +562,8 @@ describe('v2.1.0 to v1.0.0', function () {
             }, _.defaults({retainIds: false}, options), function (err, result) {
                 expect(err).to.not.be.ok;
 
-                expect(result.id).to.match(/[a-f0-9]{8}(-[a-f0-9]{4}){4}[a-f0-9]{8}/);
+                // collection ids should be retained even when retainIds is set to false
+                expect(result.id).to.equal('R1');
                 expect(result.requests).to.have.length(36);
 
 
@@ -578,7 +579,7 @@ describe('v2.1.0 to v1.0.0', function () {
             });
         });
 
-        it('should handle IDs correctly when missing', function () {
+        it('should handle IDs correctly when missing (collection id should be retained)', function () {
             transformer.convert({
                 info: {_postman_id: 'R1'},
                 item: [
@@ -592,7 +593,8 @@ describe('v2.1.0 to v1.0.0', function () {
             }, _.omit(options, ['retainIds']), function (err, result) {
                 expect(err).to.not.be.ok;
 
-                expect(result.id).to.match(/[a-f0-9]{8}(-[a-f0-9]{4}){4}[a-f0-9]{8}/);
+                // collection ids should be retained even when retainIds is set to false
+                expect(result.id).to.equal('R1');
                 expect(result.requests).to.have.length(36);
 
                 _.forEach(result.folders, function (folder) {
