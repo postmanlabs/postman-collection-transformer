2b1000cb87c48b5d01724747e7a9175a29e1c7d3
Kunal Nagpal <kunagpal@gmail.com>
2018-07-22 18:46:58 +0530

    Improved util test coverage :arrow_up:

--- test/unit/util.test.js
+++ test/unit/util.test.js
@@ -8,12 +8,51 @@ var _ = require('lodash'),
 
 /* global describe, it */
 describe('util', function () {
+    describe('handleVars', function () {
+        it('should correctly fall back to defaults where applicable', function () {
+            expect(util.handleVars({}, {
+                retainIds: true,
+                fallback: { values: [{ value: {}, disabled: 1 }] }
+            })[0]).to.deep.include({
+                type: 'any',
+                disabled: true
+            });
+        });
+    });
+
+    describe('sanitizeAuthArray', function () {
+        it('should correctly fall back to custom types when applicable', function () {
+            expect(util.sanitizeAuthArray({
+                auth: {
+                    type: 'custom',
+                    custom: [{ key: 'foo', value: {} }]
+                }
+            })).to.eql({
+                type: 'custom',
+                custom: [{ key: 'foo', value: {}, type: 'any' }]
+            });
+        });
+    });
+
+    describe('notLegacy', function () {
+        it('should bail out for falsy entities', function () {
+            expect(util.notLegacy).not.to.throw();
+            expect(util.notLegacy()).to.eql(undefined);
+        });
+
+        it('should should return true for unknown cases', function () {
+            expect(util.notLegacy).not.to.throw();
+            expect(util.notLegacy({}, 'random')).to.eql(true);
+        });
+    });
+
     describe('auth mappers', function () {
         describe('legacy', function () {
             _.forEach(util.authMappersFromLegacy, function (func, auth) {
                 it(`should handle falsy input correctly for ${auth}`, function () {
                     expect(func).to.not.throw();
                     expect(func()).to.equal(undefined);
+                    expect(func({})).to.be.ok;
                 });
             });
         });
@@ -23,6 +62,7 @@ describe('util', function () {
                 it(`should handle falsy input correctly for ${auth}`, function () {
                     expect(func).to.not.throw();
                     expect(func()).to.equal(undefined);
+                    expect(func({})).to.be.ok;
                 });
             });
         });
