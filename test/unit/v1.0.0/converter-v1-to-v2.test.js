12e32b9277f38336245df3c60acf11df4c4b7e71
Udit Vasu <udit.vasu@getpostman.com>
2018-10-26 14:55:01 +0530

    Test: Update test for null body for v1 -> v2 with retainEmptyValues

--- test/unit/v1.0.0/converter-v1-to-v2.test.js
+++ test/unit/v1.0.0/converter-v1-to-v2.test.js
@@ -2428,46 +2428,27 @@ describe('v1.0.0 to v2.0.0', function () {
             });
         });
 
-        it('should handle null request body correctly', function (done) {
-            transformer.convert({
-                id: '84b2b626-d3a6-0f31-c7a0-47733c01d0c2',
-                name: 'null-request-body',
-                order: ['4f65e265-dd38-0a67-71a5-d9dd50fa37a1'],
-                folders: [],
-                folders_order: [],
-                requests: [{
-                    id: '4f65e265-dd38-0a67-71a5-d9dd50fa37a1',
-                    url: 'https://postman-echo.com/post',
-                    method: 'POST',
-                    collectionId: '84b2b626-d3a6-0f31-c7a0-47733c01d0c2'
-                }]
-            }, options, function (err, converted) {
-                expect(err).to.not.be.ok;
+        it('should handle null request body correctly', function () {
+            transformer.convertSingle({
+                id: '9d123ce5-314a-40cd-9852-6a8569513f4e',
+                queryParams: [{ key: 'query_foo', value: 'query_bar' }]
+            }, options, function (err, result) {
+                expect(err).not.to.be.ok;
 
-                // remove `undefined` properties for testing
-                converted = JSON.parse(JSON.stringify(converted));
-
-                expect(converted).to.eql({
-                    info: {
-                        _postman_id: '84b2b626-d3a6-0f31-c7a0-47733c01d0c2',
+                expect(JSON.parse(JSON.stringify(result))).to.eql({
+                    _postman_id: '9d123ce5-314a-40cd-9852-6a8569513f4e',
+                    name: '',
+                    request: {
+                        body: null,
                         description: null,
-                        name: 'null-request-body',
-                        schema: 'https://schema.getpostman.com/json/collection/v2.0.0/collection.json'
+                        header: [],
+                        url: {
+                            query: [{ key: 'query_foo', value: 'query_bar' }],
+                            raw: ''
+                        }
                     },
-                    item: [{
-                        _postman_id: '4f65e265-dd38-0a67-71a5-d9dd50fa37a1',
-                        name: '',
-                        request: {
-                            description: null,
-                            header: [],
-                            body: null,
-                            method: 'POST',
-                            url: 'https://postman-echo.com/post'
-                        },
-                        response: []
-                    }]
+                    response: []
                 });
-                done();
             });
         });
 
