9bcf06e7b626398470201e072524089f471ba88e
Udit Vasu <udit.vasu@getpostman.com>
2018-10-30 17:32:18 +0530

    Add system tests for gitattributes

--- test/system/repository.test.js
+++ test/system/repository.test.js
@@ -185,6 +185,16 @@ describe('project repository', function () {
         });
     });
 
+    describe('.gitattributes', function () {
+        it('should exist', function (done) {
+            fs.stat('./.gitattributes', done);
+        });
+
+        it('should have readable content', function () {
+            expect(fs.readFileSync('./.gitattributes').toString()).to.be.ok;
+        });
+    });
+
     describe('.nsprc', function () {
         it('must exist', function (done) {
             fs.stat('./.nsprc', done);
