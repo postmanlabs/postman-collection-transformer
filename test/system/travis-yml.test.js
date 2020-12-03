65c6d5c56d3672f8c7b9340bebcd46a1744adfbd
Udit Vasu <udit@pm.me>
2020-12-03 23:00:23 +0530

    Update travis.yml

--- test/system/travis-yml.test.js
+++ test/system/travis-yml.test.js
@@ -22,13 +22,9 @@ describe('travis.yml', function () {
     });
 
     describe('strucure', function () {
-        it('should use the trusty Ubuntu distribution', function () {
-            expect(travisYAML.dist).to.equal('trusty');
-        });
-
         it('should have the language set to node', function () {
             expect(travisYAML.language).to.equal('node_js');
-            expect(travisYAML.node_js).to.eql(['10', '12']);
+            expect(travisYAML.node_js).to.eql([10, 12]);
         });
 
         it('should have a valid Slack notification token', function () {
