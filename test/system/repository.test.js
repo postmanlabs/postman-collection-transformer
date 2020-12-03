96995d95bfaf9073ab28f42f0163b66688075773
Udit Vasu <udit@pm.me>
2020-12-03 22:44:22 +0530

    Update npm scripts

--- test/system/repository.test.js
+++ test/system/repository.test.js
@@ -61,10 +61,11 @@ describe('project repository', function () {
 
         describe('script definitions', function () {
             it('files must exist', function () {
-                var scriptRegex = /^node\snpm\/.+\.js$/;
+                var scriptRegex = /node\snpm\/.+\.js$/;
 
                 expect(json.scripts).to.be.ok;
                 json.scripts && Object.keys(json.scripts).forEach(function (scriptName) {
+                    if (scriptName === 'test') { return; }
                     expect(scriptRegex.test(json.scripts[scriptName])).to.be.ok;
                     expect(fs.statSync('npm/' + scriptName + '.js')).to.be.ok;
                 });
@@ -72,6 +73,7 @@ describe('project repository', function () {
 
             it('must have the hashbang defined', function () {
                 json.scripts && Object.keys(json.scripts).forEach(function (scriptName) {
+                    if (scriptName === 'test') { return; }
                     var fileContent = fs.readFileSync('npm/' + scriptName + '.js').toString();
 
                     expect((/^#!\/(bin\/bash|usr\/bin\/env\snode)[\r\n][\W\w]*$/g).test(fileContent)).to.be.ok;
